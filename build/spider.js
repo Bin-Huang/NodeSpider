483e7058c7ce80c326fc2bee46fb9f92acddaa2a
git <980502459@qq.com>
2017-07-27 10:51:14 +0800

    fix the function startTask, debug of task's special info; add unit test of task's special info, and pass; move all of test file to folder test, from folder example; update the package as version 0.6.2;

--- build/spider.js
+++ build/spider.js
@@ -235,7 +235,7 @@ function startTask(type, task, self) {
     // task可以携带info，此时将覆盖plan设置中的info。如果task没有info，则使用plan中的
     const info = task.info;
     if (typeof info === "undefined" || info === null) {
-        task.info = plan.option.info;
+        task.info = plan.info;
     }
     self._STATE.currentConnections[type]++;
     self._STATE.currentTotalConnections++;
