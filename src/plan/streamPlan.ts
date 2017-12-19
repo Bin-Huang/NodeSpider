10fc4299cd261deac5b25f3194862180698f06de
Ben Huang <tohuangbin@gmail.com>
2017-12-19 14:31:31 +0800

    modify streamPlan: add function `done` in params `current` to control task's process;

--- src/plan/streamPlan.ts
+++ src/plan/streamPlan.ts
@@ -11,7 +11,8 @@ export interface IStreamPlanOption {
 
 // current crawl task; for `rule` function in the plan
 export interface IStreamPlanCurrent extends ITask {
-    res: request.Request;
+    stream: request.Request | null;
+    done: () => void;
     [propName: string]: any;
 }
 
@@ -29,9 +30,10 @@ export class StreamPlan implements IPlan {
     }
     public process(task: ITask, spider: Spider) {
         return new Promise((resolve, reject) => {
-            const requestOpts = Object.assign({url: task.url}, this.option.headers);
+            const done = () => resolve();
 
-            let res: request.Request;
+            const requestOpts = Object.assign({url: task.url}, this.option.headers);
+            let res: request.Request|null;
             let err: Error|null = null;
             try {
                 res = request({
@@ -40,22 +42,19 @@ export class StreamPlan implements IPlan {
                     method: this.option.method,
                     url: task.url,
                 });
-                (res as request.Request).on("complete", resolve);
-                (res as request.Request).on("error", resolve);
             } catch (e) {
                 err = e;
-                resolve();
+                res = null;
             }
             const current = {
                 ... task,
-                res,    // TODO B
+                done,
+                stream: res,    // TODO B
             };
             // 为什么不直接监听request的close事件以resolve？
             // 当req流关闭时，下游可能还有操作，此时不能直接resolve进入下一个任务
             // 所以要把resovle当前任务的工作交给开发者自行决定
             this.option.callback(err, current, spider);
-
-            // 当请求流结束或错误，即应该认为这次任务是执行完全的
         });
     }
 }
