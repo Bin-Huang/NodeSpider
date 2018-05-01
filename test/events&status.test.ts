8a38b7382944da656e12b79cc8742a6166e98221
Ben Huang <tohuangbin@gmail.com>
2018-05-01 12:37:45 +0800

    Add unit testing for spider status

--- test/events&status.test.ts
+++
@@ -1,9 +0,0 @@
-import test from "ava";
-import * as delay from "delay";
-import * as sinon from "sinon";
-import { Spider } from "../src/index";
-import { IPlan } from "../src/interfaces";
-
-test("test for status", async (t) => {
-  t.is(1, 1);
-});
