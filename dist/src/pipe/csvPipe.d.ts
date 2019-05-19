5b03e648dfdf3ec3a0837756a3de493fb67cfb0d
Bin-Huang <tohuangbin@gmail.com>
2019-05-19 09:56:39 +0800

    Remove dist from git registory

--- dist/src/pipe/csvPipe.d.ts
+++
@@ -1,7 +0,0 @@
-import { IPipe, IPipeItems } from "../interfaces";
-export interface ICsvPipeOpts {
-    name: string;
-    path: string;
-    items: IPipeItems;
-}
-export default function csvPipe(opts: ICsvPipeOpts): IPipe;
