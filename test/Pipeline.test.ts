e2e7ab61f01f84f2e743b46776198804260a52bf
Bin-Huang <tohuangbin@gmail.com>
2019-08-02 17:11:15 +0800

    Tweak unit test

--- test/Pipeline.test.ts
+++ test/Pipeline.test.ts
@@ -1,17 +1,6 @@
 import test from "ava";
 import Pipeline from '../src/Pipeline'
 
-test('Test Pipeline scaffold', async (t) => {
-  let data = { start: 0 }
-  const pl = new Pipeline((d) => ({ ...d, p1: true }))
-    .to((d) => ({ ...d, p1: true, p2: true }))
-    .to(new Pipeline((d) => ({ ...d, p1: true, p3: true })))
-    .to(new Pipeline((d) => ({ ...d, p1: true, p4: true })))
-    .to(new Pipeline(new Pipeline((d) => ({ ...d, p1: true, p5: true }))))
-    .to((d) => t.deepEqual(d, { ...data, p1: true, p2: true, p3: true, p4: true, p5: true }))
-  await pl.save(data)
-})
-
 test('Test Pipeline processing sequence', async (t) => {
   let data = { s: '0' }
   const pl = new Pipeline((d: {s: string}) => ({ s: d.s + '1' }))
